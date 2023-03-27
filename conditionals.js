7f35a4764c7dcfc210796d80bd2d9edbe732f791
biniam <97873943+calgarybino@users.noreply.github.com>
2023-03-26 22:53:11 -0400

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+if (raining) console.log("Don't forget your umbrella!!!");
+if (cold) console.log("Make sure you pick out a scarf");
+console.log("now you are ready to go outside!!");
+const isCitizen = true;
+const age = 26;
+if (isCitizen && age > 18) console.log("You are elibile to vote. ");
+const temperature = 30;
+if (temperature < -40 || temperature > 40)
+  console.log("Maybe going outside isn't such a great idea..");
+if (!raining) console.log("leave your umbrella at home!!! ");
